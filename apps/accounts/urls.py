2828f7ab57af6d1ae36f30d63cd3835d6bbb55ad
ankush826921-crypto <ankush826921@gmail,com>
2026-09-17 21:48:27 +0530

    Add by Deep login/Register page

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,4 +1,44 @@
 from django.urls import path
+from . import views
+
+
+app_name = "accounts"
+
 
 urlpatterns = [
+    path(
+        "login/",
+        views.login_view,
+        name="login",
+    ),
+
+    path(
+        "register/",
+        views.register_view,
+        name="register",
+    ),
+
+    path(
+        "forgot-password/",
+        views.forgot_password_view,
+        name="forgot_password",
+    ),
+
+    path(
+        "verify-otp/",
+        views.verify_otp_view,
+        name="verify_otp",
+    ),
+
+    path(
+        "reset-password/",
+        views.reset_password_view,
+        name="reset_password",
+    ),
+
+    path(
+        "password-success/",
+        views.password_success_view,
+        name="password_success",
+    ),
 ]
