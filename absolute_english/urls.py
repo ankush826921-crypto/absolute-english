d7adca4d3b4d997ec4d7d2e1a2c677ac708faa7e
Student Vishu <student@vishu.in>
2026-09-18 10:38:07 +0530

    Add American and British English language pages

--- absolute_english/urls.py
+++ absolute_english/urls.py
@@ -15,6 +15,9 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
     path('book-demo/', views.book_demo, name='book_demo'),
     path('enroll/', views.enroll, name='enroll'),
+    path('american/',views.american,name='american'),
+    path('british/',views.british,name='british'),
+
 ]
 
 if settings.DEBUG:
